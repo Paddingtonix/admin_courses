122b77709869698b82dbb9e286165b90f1247ce2
Paddingtonix <108619947+Paddingtonix@users.noreply.github.com>
2024-08-14 18:28:29 +0700

    000

--- pages/course-content.vue
+++ pages/course-content.vue
@@ -83,7 +83,56 @@
                     <span class="oil-course-content__test__general-settings__name">{{ setting.name }}</span>
                     <div class="oil-course-content__test__general-settings__value">
                         <span>{{ setting.value.length ? setting.value.length : 'Нет данных' }}</span>
-                        <span>{{ setting.desc }}</span>
+                        <span v-if="!setting.value.length">{{ setting.desc }}</span>
+                    </div>
+                </div>
+                <div 
+                    class="oil-course-content__attention"
+                    @click="openSummary"
+                >
+                    <div class="oil-course-content__attention__head">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M12 16V12M12 8H12.01M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" stroke="#323C46" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <span>Как работать с содержанием теста?</span>
+                        <svg class="oil-course-content__attention__chevron" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6 9L12 15L18 9" stroke="#374351" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
+                    <div v-if="visible_simmary.value" class="oil-course-content__attention__text">
+                        <div class="oil-course-content__attention__text__frame">
+                            <span>Структура теста</span>
+                            <ul>
+                                <li>Тест должен содержать хотя бы один вопрос. Наполнение вопроса включает в себя следующие блоки:</li>
+                                <ul>
+                                    <li>
+                                        направление вопроса
+                                    </li>
+                                    <li>
+                                        тело вопроса
+                                    </li>
+                                    <li>
+                                        четыре варианта ответа
+                                    </li>
+                                    <li>
+                                        балл за правильный ответ.
+                                    </li>
+                                </ul>
+                                <li>Проходной балл указывается отдельно от вопросов (общий для всего теста).</li>
+                            </ul>
+                        </div>
+                        <div class="oil-course-content__attention__text__frame">
+                            <span>Добавление нового вопроса</span>
+                            <ul>
+                                <li>Чтобы добавить новый вопрос, наведите курсор на структуру теста и нажмите на кнопку “Добавить вопрос”, он появится в общем списке вопросов и станет доступным для наполнения.</li>
+                            </ul>
+                        </div>
+                        <div class="oil-course-content__attention__text__frame">
+                            <span>Внимание!</span>
+                            <ul>
+                                <li>Убедитесь в том, что вы выбрали направления курса во вкладке “Общие настройки курса”, так как тема каждого вопроса должна соответствовать одному из направлений курса.</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -193,7 +242,20 @@ export default defineComponent({
 
     &__test
         &__general-settings
-            // @include flex_center()
+            @include flex_start()
+            &__value
+                @include flex_column()
+                gap: rem(2)
+                span 
+                    &:first-child
+                        font-weight: bold
+                    
+                    &:last-child
+                        font-size: rem(12)
+            
+            &__name 
+                padding: rem(28) rem(108) rem(28) rem(8)
+                min-width: rem(360)
 
 
     &__btn 
