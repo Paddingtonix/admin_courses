a481c4f04178087d080acb82fa1e1ba104b63d72
Aspors <tmieoml@gmail.com>
2024-09-20 03:48:04 +0700

    доделал хук валидации

--- src/components/ui-components/forms/form-tags.vue
+++ src/components/ui-components/forms/form-tags.vue
@@ -1,67 +1,67 @@
 <template>
-	<form class="add-tag" action="#">
-		<MarkSelector
-			v-if="headers.length"
-			:objectList="headers"
-			class="add-tag__selector"
-			label="Раздел"
-			:choosed_variable="{
-				name: tagForm.headingName,
-				id: tagForm.headingId,
-			}"
-			@select-object="setHeading($event)"
-		></MarkSelector>
-		<span class="add-tag__no-selector" v-else>
-			Вы должны создать раздел, прежде, чем создать метку
-		</span>
-		<InputCmp
-			@set_value="setFormValue(tagFormEnum._name, $event)"
-			:modelValue="tagForm.name"
-			:error="errors.name ? errors.name : ''"
-			class="add-tag__input"
-			label="Название метки"
-		></InputCmp>
-		<LangSwitcherCmp
-			:active="activeLang"
-			class="add-tag__lang-switcher"
-			@change-lang="changeLang($event)"
-		/>
-		<TextareaCmp
-			class="add-tag__text-area"
-			label="Перевод метки"
-			:modelValue="tagForm.localizations[activeLang]"
-			:error="errors[activeLang] ?? ''"
-			@set_textarea="
-				setFormValue(tagFormEnum._localizations, $event, activeLang)
-			"
-		></TextareaCmp>
-		<TooltipCmp
-			class="add-tag__tooltip"
-			text="Перевод метки - это текст, который будет отображаться на сайте. Метку необходимо перевести на все обязательные языки (помечены звёздочкой)"
-		/>
-		<div class="add-tag__button-wrapper">
-			<BtnCmp
-				type="button"
-				background_type="_secondary"
-				text="Отмена"
-				@click="closeModal()"
-			></BtnCmp>
-			<BtnCmp
-				v-if="!modalData.modalProps.edit"
-				type="submit"
-				text="Добавить"
-				:disabled="!isFormValid"
-				@click.prevent="sendFormTags"
-			></BtnCmp>
-			<BtnCmp
-				v-if="modalData.modalProps.edit"
-				type="submit"
-				text="Сохранить"
-				:disabled="!isFormValid"
-				@click.prevent="patchFormTags"
-			></BtnCmp>
-		</div>
-	</form>
+    <form class="add-tag" action="#">
+        <MarkSelector
+            v-if="headers.length"
+            :objectList="headers"
+            class="add-tag__selector"
+            label="Раздел"
+            :choosed_variable="{
+                name: tagForm.headingName,
+                id: tagForm.headingId,
+            }"
+            @select-object="setHeading($event)"
+        ></MarkSelector>
+        <span class="add-tag__no-selector" v-else>
+            Вы должны создать раздел, прежде, чем создать метку
+        </span>
+        <InputCmp
+            @set_value="setFormValue(tagFormEnum._name, $event)"
+            :modelValue="tagForm.name"
+            :error="errors.name ? errors.name : ''"
+            class="add-tag__input"
+            label="Название метки"
+        ></InputCmp>
+        <LangSwitcherCmp
+            :active="activeLang"
+            class="add-tag__lang-switcher"
+            @change-lang="changeLang($event)"
+        />
+        <TextareaCmp
+            class="add-tag__text-area"
+            label="Перевод метки"
+            :modelValue="tagForm.localizations[activeLang]"
+            :error="errors[activeLang] ?? ''"
+            @set_textarea="
+                setFormValue(tagFormEnum._localizations, $event, activeLang)
+            "
+        ></TextareaCmp>
+        <TooltipCmp
+            class="add-tag__tooltip"
+            text="Перевод метки - это текст, который будет отображаться на сайте. Метку необходимо перевести на все обязательные языки (помечены звёздочкой)"
+        />
+        <div class="add-tag__button-wrapper">
+            <BtnCmp
+                type="button"
+                background_type="_secondary"
+                text="Отмена"
+                @click="closeModal()"
+            ></BtnCmp>
+            <BtnCmp
+                v-if="!modalData.modalProps.edit"
+                type="submit"
+                text="Добавить"
+                :disabled="!isFormValid"
+                @click.prevent="sendFormTags"
+            ></BtnCmp>
+            <BtnCmp
+                v-if="modalData.modalProps.edit"
+                type="submit"
+                text="Сохранить"
+                :disabled="!isFormValid"
+                @click.prevent="patchFormTags"
+            ></BtnCmp>
+        </div>
+    </form>
 </template>
 
 <script lang="ts" setup>
@@ -70,7 +70,6 @@ import { useTagsStore } from "~/src/stores/storeTags";
 import type { IFormTags } from "~/src/ts-interface/storeModal.type";
 import type { IHeading, ITags } from "~/src/ts-interface/storeTags.type";
 import { useFormValidate } from "~/src/utils/useFormValidate";
-import { validateForm } from "~/src/utils/validateForm";
 import { validation_schemas } from "./validation-schemas/modal-schemas.schema";
 
 const { tags_form_schema } = validation_schemas;
@@ -88,77 +87,88 @@ const headers = modalData.modalProps?.headers ?? [];
 const activeLang = ref("ru");
 
 const initialTagForm: ITags = {
-	headingId: null,
-	headingName: "",
-	id: 0,
-	isTranslated: null,
-	name: "",
-	localizations: {
-		ru: "",
-		en: "",
-		test: "",
-	},
+    headingId: null,
+    headingName: "",
+    id: 0,
+    isTranslated: null,
+    name: "",
+    localizations: {
+        ru: "",
+        en: "",
+        test: "",
+    },
 };
 
 const tagForm: ITags = reactive(
-	modalData.modalProps?.tagForm || initialTagForm
+    modalData.modalProps?.tagForm || initialTagForm
 );
 
 enum tagFormEnum {
-	_name = "name",
-	_localizations = "localizations",
+    _name = "name",
+    _localizations = "localizations",
 }
 const { validateOnSubmit, isFormValid, setCustomError, errors } =
-	useFormValidate(tagForm, tags_form_schema);
+    useFormValidate(tagForm, tags_form_schema);
 
 const setFormValue = (
-	_fieldName: keyof Omit<
-		ITags,
-		"headingId" | "headingName" | "id" | "isTranslated"
-	>,
-	inputValue: { value: string; type: string },
-	localization?: string
+    _fieldName: keyof Omit<
+        ITags,
+        "headingId" | "headingName" | "id" | "isTranslated"
+    >,
+    inputValue: { value: string; type: string },
+    localization?: string
 ) => {
-	if (_fieldName === "localizations" && localization) {
-		tagForm[_fieldName][localization] = inputValue.value;
-	} else if (_fieldName !== "localizations") {
-		tagForm[_fieldName] = inputValue.value;
-	}
+    if (_fieldName === "localizations" && localization) {
+        tagForm[_fieldName][localization] = inputValue.value;
+    } else if (_fieldName !== "localizations") {
+        tagForm[_fieldName] = inputValue.value;
+    }
 };
 
 const changeLang = (newLang: string) => {
-	activeLang.value = newLang;
+    activeLang.value = newLang;
 };
 
 const setHeading = (data: IHeading) => {
-	tagForm.headingId = data.id;
-	tagForm.headingName = data.name;
+    tagForm.headingId = data.id;
+    tagForm.headingName = data.name;
 };
 
 const sendFormTags = () => {
-	validateOnSubmit();
-	if (isFormValid.value) {
-		tagStore.postTag(tagForm).then(() => {
-			storeModal.closeModal();
-		});
-	}
+    try {
+        validateOnSubmit();
+        if (isFormValid.value) {
+            tagStore.postTag(tagForm).then(() => {
+                storeModal.closeModal();
+            });
+        }
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 const patchFormTags = () => {
-	const formData = {
-		headingId: tagForm.headingId,
-		headingName: tagForm.headingName,
-		name: tagForm.name,
-		localizations: tagForm.localizations,
-	};
-	tagStore
-		.patchTag(formData, tagForm.id)
-		.then(() => {
-			storeModal.closeModal();
-		})
-		.catch((err) => {
-			console.warn(err);
-		});
+    try {
+        validateOnSubmit();
+        if (isFormValid.value) {
+            const formData = {
+                headingId: tagForm.headingId,
+                headingName: tagForm.headingName,
+                name: tagForm.name,
+                localizations: tagForm.localizations,
+            };
+            tagStore
+                .patchTag(formData, tagForm.id)
+                .then(() => {
+                    storeModal.closeModal();
+                })
+                .catch((err) => {
+                    console.warn(err);
+                });
+        }
+    } catch (e) {
+        console.log(e);
+    }
 };
 </script>
 
--- src/utils/useFormValidate.ts
+++ src/utils/useFormValidate.ts
@@ -1,193 +1,204 @@
 import { deepEqual } from "./deepComparsion";
-import { reactive, computed } from "vue";
+import { reactive, computed, watch } from "vue";
 
 interface IValidationRule {
-	required?: { errorMessage: string };
-	min?: { errorMessage?: string; minValue: number };
-	max?: { errorMessage?: string; maxValue: number };
-	shouldChange?: { errorMessage: string };
-	validateNumber?: { errorMessage?: string; range: [number, number] };
-	isName?: { errorMessage: string };
-	isObject?: { validationSchema: IValidationRule };
-	defaultError: string;
+    required?: { errorMessage: string };
+    min?: { errorMessage?: string; minValue: number };
+    max?: { errorMessage?: string; maxValue: number };
+    shouldChange?: { errorMessage: string };
+    validateNumber?: { errorMessage?: string; range: [number, number] };
+    validateString?: { errorMessage: string; regExp: RegExp };
+    isObject?: { validationSchema: IValidationRule };
+    defaultError: string;
 }
 
 export interface IValidationSchema {
-	[key: string]: IValidationRule;
+    [key: string]: IValidationRule;
 }
 
 interface ICurrentForm {
-	[key: string]: any;
+    [key: string]: any;
 }
 
 export const useFormValidate = (
-	currentForm: ICurrentForm,
-	validationSchema: IValidationSchema
+    currentForm: ICurrentForm,
+    validationSchema: IValidationSchema
 ) => {
-	const initialForm = JSON.parse(JSON.stringify(currentForm));
+    const initialForm = JSON.parse(JSON.stringify(currentForm));
+    const errors = reactive<{ [key: string]: string }>({});
 
-	const errors = reactive<{
-		[key: string]: string;
-	}>({});
+    const isFormContainsObject = computed(() =>
+        Object.keys(validationSchema).filter(
+            (field) => validationSchema[field]?.isObject
+        )
+    );
 
-	const isNameRegExp = /^[A-Za-z0-9!@#$%^&*()_+={}\[\]:;"'<>,.?\/\\|`~\-]+$/;
+    const watchFieldChanges = () => {
+        isFormContainsObject.value.forEach((fieldInner) => {
+            if (typeof currentForm[fieldInner] === "object") {
+                Object.keys(currentForm[fieldInner]).forEach((lang) => {
+                    watch(
+                        () => currentForm[fieldInner][lang],
+                        () => {
+                            validateField(
+                                lang,
+                                validationSchema[fieldInner].isObject
+                                    ?.validationSchema
+                            );
+                            clearShouldChangeErrors();
+                        }
+                    );
+                });
+            }
+        });
 
-	const cyrillycNameRegExp =
-		/^[^[А-Яа-я0-9!@#$%^&*()_+={}\[\]:;"'<>,.?\/\\|`~\-]+$]/;
+        Object.keys(currentForm).forEach((field) => {
+            watch(
+                () => currentForm[field],
+                () => {
+                    validateField(field);
+                    clearShouldChangeErrors();
+                }
+            );
+        });
+    };
 
-	const isFormContainsObject = computed(() => {
-		return Object.keys(validationSchema).reduce<string[]>((acc, field) => {
-			if (validationSchema[field]?.isObject) {
-				acc.push(field);
-			}
-			return acc;
-		}, []);
-	});
+    const checkRules = (
+        rules: IValidationRule,
+        fieldValue: string | number
+    ): string | null => {
+        const errorChecks: Record<string, () => string | null> = {
+            required: () =>
+                typeof fieldValue !== "object" &&
+                rules?.required?.errorMessage &&
+                !fieldValue
+                    ? rules.required.errorMessage
+                    : null,
+            validateString: () => {
+                const currentRegExp = rules.validateString?.regExp;
 
-	if (isFormContainsObject.value.length) {
-		for (const fieldInner of isFormContainsObject.value) {
-			if (
-				fieldInner &&
-				currentForm[fieldInner] &&
-				typeof currentForm[fieldInner] === "object"
-			) {
-				Object.keys(currentForm[fieldInner]).forEach((lang) => {
-					watch(
-						() => currentForm[fieldInner][lang],
-						() => {
-							console.log(
-								"changingField: ",
-								currentForm[fieldInner][lang]
-							);
+                return currentRegExp && typeof fieldValue === "string"
+                    ? !currentRegExp.test(fieldValue)
+                        ? rules.validateString?.errorMessage ||
+                          rules.defaultError
+                        : null
+                    : null;
+            },
 
-							validateField(
-								lang,
-								validationSchema[fieldInner].isObject
-									?.validationSchema
-							);
-							clearShouldChangeErrors();
-						}
-					);
-				});
-			}
-		}
-	}
+            min: () =>
+                typeof fieldValue === "string" &&
+                rules.min?.minValue &&
+                fieldValue.length < rules.min.minValue
+                    ? rules.min.errorMessage || rules.defaultError
+                    : null,
 
-	// Следим за изменениями каждого поля
-	Object.keys(currentForm).forEach((field) => {
-		watch(
-			() => currentForm[field],
-			() => {
-				validateField(field); // Валидируем это конкретное поле
-				clearShouldChangeErrors(); // Убираем ошибки shouldChange со всех полей
-			}
-		);
-	});
+            max: () =>
+                typeof fieldValue === "string" &&
+                rules.max?.maxValue &&
+                fieldValue.length > rules.max.maxValue
+                    ? rules.max.errorMessage || rules.defaultError
+                    : null,
 
-	// Валидация конкретного поля
-	const validateField = (field: string, customSchema?: IValidationRule) => {
-		const fieldValue = !customSchema
-			? currentForm[field]
-			: currentForm[isFormContainsObject.value[0]][field];
-		const rules = !customSchema ? validationSchema[field] : customSchema;
-		console.log(errors);
+            validateNumber: () => {
+                if (!rules?.validateNumber) {
+                    return null;
+                }
+                const [min, max] = rules.validateNumber?.range;
+                const valueAsNumber = Number(fieldValue);
+                return rules.validateNumber &&
+                    (isNaN(valueAsNumber) ||
+                        valueAsNumber < min ||
+                        valueAsNumber > max)
+                    ? rules.validateNumber.errorMessage || rules.defaultError
+                    : null;
+            },
 
-		// Убираем старую ошибку для данного поля
-		delete errors[field];
+            isObject: () => {
+                if (
+                    rules?.isObject?.validationSchema &&
+                    typeof fieldValue === "object" &&
+                    fieldValue !== null
+                ) {
+                    return Object.keys(fieldValue).reduce<string | null>(
+                        (acc, key) => {
+                            return (
+                                acc ||
+                                checkRules(
+                                    rules.isObject!.validationSchema,
+                                    fieldValue[key]
+                                )
+                            );
+                        },
+                        null
+                    );
+                }
+                return null;
+            },
 
-		// Обязательное поле
-		if (rules?.required && !fieldValue) {
-			errors[field] = rules.required.errorMessage;
-			return;
-		}
+            shouldChange: () =>
+                typeof fieldValue === "string" &&
+                fieldValue.trim() !== "" &&
+                deepEqual(initialForm, currentForm)
+                    ? rules.shouldChange?.errorMessage || rules.defaultError
+                    : null,
+        };
 
-		// Минимальная длина
-		if (
-			rules?.min?.minValue !== undefined &&
-			fieldValue.length < rules.min.minValue
-		) {
-			errors[field] = rules.min?.errorMessage || rules.defaultError;
-		}
+        return (
+            Object.values(errorChecks)
+                .map((check) =>
+                    rules
+                        ? Object.keys(rules).includes(check.name)
+                            ? check()
+                            : null
+                        : null
+                )
+                .filter((error): error is string => error !== null)
+                .shift() || null
+        );
+    };
 
-		// Максимальная длина
-		if (
-			rules?.max?.maxValue !== undefined &&
-			fieldValue.length > rules.max.maxValue
-		) {
-			errors[field] = rules.max?.errorMessage || rules.defaultError;
-		}
+    const validateField = (
+        field: string,
+        customSchema?: IValidationRule
+    ): void => {
+        const rules = customSchema || validationSchema[field];
+        const fieldValue = customSchema
+            ? currentForm[isFormContainsObject.value[0]][field] // я тут ишачил, как мог, но пока что это не совсем универсальная валидация. Если вложенных объектов будет больше, то пизда))
+            : currentForm[field];
 
-		if (rules?.isName && !isNameRegExp.test(fieldValue)) {
-			errors[field] = rules.isName.errorMessage || rules.defaultError;
-		}
-		// Валидация числового значения
-		if (rules?.validateNumber) {
-			const [min, max] = rules.validateNumber.range;
-			const valueAsNumber = Number(fieldValue);
-			if (
-				isNaN(valueAsNumber) ||
-				valueAsNumber < min ||
-				valueAsNumber > max
-			) {
-				errors[field] =
-					rules.validateNumber?.errorMessage || rules.defaultError;
-			}
-		}
+        const error = checkRules(rules, fieldValue);
 
-		if (rules?.isObject && rules.isObject?.validationSchema) {
-			if (typeof fieldValue === "object" && fieldValue !== null) {
-				console.log("fieldValue is an object:", fieldValue);
-				Object.keys(fieldValue).forEach((key) => {
-					console.log("Validating nested field:", key);
-					validateField(key, rules.isObject?.validationSchema);
-				});
-			} else {
-				console.warn("fieldValue is not an object:", fieldValue);
-			}
-		}
+        error ? (errors[field] = error) : delete errors[field];
+    };
 
-		// Проверка на изменение формы, если поле не пустое
-		if (
-			rules?.shouldChange &&
-			fieldValue.trim() !== "" &&
-			deepEqual(initialForm, currentForm)
-		) {
-			errors[field] =
-				rules.shouldChange?.errorMessage || rules.defaultError;
-		}
-	};
+    const clearShouldChangeErrors = () =>
+        !deepEqual(initialForm, currentForm) &&
+        Object.keys(validationSchema).forEach((field) => {
+            const shouldChangeError =
+                validationSchema[field].shouldChange?.errorMessage;
+            errors[field] === shouldChangeError && delete errors[field];
+        });
 
-	// Убираем ошибки по shouldChange для всех полей, если хоть одно поле изменилось
-	const clearShouldChangeErrors = () => {
-		if (!deepEqual(initialForm, currentForm)) {
-			Object.keys(validationSchema).forEach((field) => {
-				if (validationSchema[field].shouldChange) {
-					// Убираем ошибку shouldChange только для этого правила
-					if (
-						errors[field] ===
-						validationSchema[field].shouldChange?.errorMessage
-					) {
-						delete errors[field];
-					}
-				}
-			});
-		}
-	};
+    const validateOnSubmit = () => {
+        Object.keys(currentForm).forEach((field) =>
+            typeof currentForm[field] !== "object" ? validateField(field) : null
+        );
+        isFormContainsObject.value.forEach((fieldInner) => {
+            Object.keys(currentForm[fieldInner]).forEach((lang) => {
+                validateField(
+                    lang,
+                    validationSchema[fieldInner].isObject?.validationSchema
+                );
+            });
+        });
+    };
 
-	// Полная валидация перед отправкой формы
-	const validateOnSubmit = () => {
-		Object.keys(currentForm).forEach((field) => {
-			validateField(field);
-		});
-	};
+    const setCustomError = (field: string, msg: string) =>
+        (errors[field] = msg);
 
-	const setCustomError = (field: string, msg: string) => {
-		errors[field] = msg;
-	};
+    const isFormValid = computed(() => !Object.keys(errors).length);
 
-	const isFormValid = computed(() => {
-		return Object.keys(errors).length === 0;
-	});
+    watchFieldChanges();
 
-	return { isFormValid, errors, validateOnSubmit, setCustomError };
+    return { isFormValid, errors, validateOnSubmit, setCustomError };
 };
