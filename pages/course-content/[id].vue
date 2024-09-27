b7069c1cf55000d612cde66a14524d04b5518afe
Aspors <tmieoml@gmail.com>
2024-09-27 17:24:52 +0700

    поправил курс-контент

--- pages/course-content/[id].vue
+++ pages/course-content/[id].vue
@@ -1,41 +1,41 @@
 <template>
-    <section class="oil-container">
-        <div class="oil-course-content">
-            <breadCmp
-                :prev_page="['Курсы', `${course_settings.title_course}`]"
-                :current_page="courseContentStore.generalSettings.title"
-                class="oil-course-content__bread"
-            />
-            <AttentionMessage
-                v-if="courseStore.status === 'Archived'"
-                message="Курс архивирован, вы не можете просмотреть его наполнение на сайте. Доступна опция выгрузки курса на ПК в формате PDF."
-                :buttonText="'Скачать PDF'"
-                :buttonClick="() => {}"
-            />
-            <ContentModule v-if="course_settings.type_course === 'text'" />
-            <TestSection
-                :questions="courseContentStore.questions"
-                :id="id"
-                v-else-if="content === 'test' && !isLoading"
-            >
-                <template #summary-text>
-                    <div
-                        v-for="section in summarySections"
-                        :key="section.title"
-                        class="oil-course-content__attention__text__frame"
-                    >
-                        <span>{{ section.title }}</span>
-                        <ul>
-                            <li v-for="item in section.content" :key="item">
-                                {{ item }}
-                            </li>
-                        </ul>
-                    </div>
-                </template>
-            </TestSection>
-            <div v-else-if="isLoading">loading</div>
-        </div>
-    </section>
+	<section class="oil-container">
+		<div class="oil-course-content">
+			<breadCmp
+				:prev_page="['Курсы', `${course_settings.title_course}`]"
+				:current_page="courseContentStore.generalSettings.title"
+				class="oil-course-content__bread"
+			/>
+			<AttentionMessage
+				v-if="courseStore.status === 'Archived'"
+				message="Курс архивирован, вы не можете просмотреть его наполнение на сайте. Доступна опция выгрузки курса на ПК в формате PDF."
+				:buttonText="'Скачать PDF'"
+				:buttonClick="() => {}"
+			/>
+			<ContentModule v-if="course_settings.type_course === 'text'" />
+			<TestSection
+				:questions="courseContentStore.questions"
+				:id="id"
+				v-else-if="content === 'test' && !isLoading"
+			>
+				<template #summary-text>
+					<div
+						v-for="section in summarySections"
+						:key="section.title"
+						class="oil-course-content__attention__text__frame"
+					>
+						<span>{{ section.title }}</span>
+						<ul>
+							<li v-for="item in section.content" :key="item">
+								{{ item }}
+							</li>
+						</ul>
+					</div>
+				</template>
+			</TestSection>
+			<div v-else-if="isLoading">loading</div>
+		</div>
+	</section>
 </template>
 
 <script lang="ts">
@@ -46,80 +46,75 @@ import { useRoute } from "vue-router";
 import type { ICourseContentQuestions } from "~/src/ts-interface/course-content";
 
 export default defineComponent({
-    props: {
-        content: {
-            type: String,
-            default: "test", //text
-        },
-    },
-    setup() {
-        const courseStore = useStoreCourses();
-        const courseContentStore = useStoreCourseContent();
-        const route = useRoute();
-        const { id } = route.params as unknown as { id: string };
-        const course_settings = reactive({
-            title_course: "",
-            type_course: "",
-        });
+	props: {
+		content: {
+			type: String,
+			default: "test", //text
+		},
+	},
+	setup() {
+		const courseStore = useStoreCourses();
+		const courseContentStore = useStoreCourseContent();
+		const route = useRoute();
+		const { id } = route.params as unknown as { id: string };
+		const course_settings = reactive({
+			title_course: "",
+			type_course: "",
+		});
 
-        const summarySections = [
-            {
-                title: "Структура теста",
-                content: [
-                    "Тест должен содержать хотя бы один вопрос. Наполнение вопроса включает в себя следующие блоки:",
-                    "направление вопроса",
-                    "тело вопроса",
-                    "четыре варианта ответа",
-                    "балл за правильный ответ.",
-                    "Проходной балл указывается отдельно от вопросов (общий для всего теста).",
-                ],
-            },
-            {
-                title: "Добавление нового вопроса",
-                content: [
-                    "Чтобы добавить новый вопрос, наведите курсор на структуру теста и нажмите на кнопку 'Добавить вопрос', он появится в общем списке вопросов и станет доступным для наполнения.",
-                ],
-            },
-            {
-                title: "Внимание!",
-                content: [
-                    "Убедитесь в том, что вы выбрали направления курса во вкладке 'Общие настройки курса', так как тема каждого вопроса должна соответствовать одному из направлений курса.",
-                ],
-            },
-        ];
-        const isLoading = ref(true);
-        onMounted(() => {
-            nextTick(() => {
-                courseContentStore
-                    .getCourseContent(id)
-                    .catch((error) => {
-                        console.error(
-                            "Ошибка при загрузке содержания курса:",
-                            error
-                        );
-                    })
-                    .finally(() => {
-                        isLoading.value = false;
-                        if (route.query.length) {
-                            course_settings.title_course = route.query[""][0];
-                            course_settings.type_course = route.query[""][1];
-                        } else {
-                            course_settings.title_course = "no-title";
-                            course_settings.type_course = "no-type";
-                        }
-                    });
-            });
-        });
+		const summarySections = [
+			{
+				title: "Структура теста",
+				content: [
+					"Тест должен содержать хотя бы один вопрос. Наполнение вопроса включает в себя следующие блоки:",
+					"направление вопроса",
+					"тело вопроса",
+					"четыре варианта ответа",
+					"балл за правильный ответ.",
+					"Проходной балл указывается отдельно от вопросов (общий для всего теста).",
+				],
+			},
+			{
+				title: "Добавление нового вопроса",
+				content: [
+					"Чтобы добавить новый вопрос, наведите курсор на структуру теста и нажмите на кнопку 'Добавить вопрос', он появится в общем списке вопросов и станет доступным для наполнения.",
+				],
+			},
+			{
+				title: "Внимание!",
+				content: [
+					"Убедитесь в том, что вы выбрали направления курса во вкладке 'Общие настройки курса', так как тема каждого вопроса должна соответствовать одному из направлений курса.",
+				],
+			},
+		];
+		const isLoading = ref(true);
+		onMounted(() => {
+			nextTick(() => {
+				courseContentStore
+					.getCourseContent(id)
+					.catch((error) => {
+						console.error(
+							"Ошибка при загрузке содержания курса:",
+							error
+						);
+					})
+					.finally(() => {
+						isLoading.value = false;
+						course_settings.title_course = route.query[""][0];
+						course_settings.type_course = route.query[""][1];
+					});
+			});
+		});
 
-        return {
-            courseStore,
-            courseContentStore,
-            course_settings,
-            summarySections,
-            id,
-            isLoading,
-        };
-    },
+		return {
+			courseStore,
+			courseContentStore,
+			course_settings,
+			summarySections,
+			id,
+			isLoading,
+		};
+	},
 });
 </script>
 
--- src/stores/storeCourseContent.ts
+++ src/stores/storeCourseContent.ts
@@ -1,96 +1,95 @@
 import axios, { type AxiosResponse } from "axios";
 import { defineStore } from "pinia";
 import type {
-    ICourseContent,
-    ICourseContentQuestions,
-    IDirection,
-    IDirectionResponse,
-    IGeneralCourseSettings,
+	ICourseContent,
+	ICourseContentQuestions,
+	IDirection,
+	IDirectionResponse,
+	IGeneralCourseSettings,
 } from "../ts-interface/course-content";
 
 export const useStoreCourseContent = defineStore("course-content", {
-    state: () => ({
-        courseId: 0,
-        testType: "",
-        generalSettings: {} as IGeneralCourseSettings,
-        directions: [] as IDirection[],
-        questions: [] as ICourseContentQuestions[],
-    }),
-    actions: {
-        async getDirections(courseId: number) {
-            return axios
-                .get(`admin/v1/Course/${courseId}`)
-                .then((response) => {
-                    const data = response.data as IDirectionResponse;
-                    this.directions = data.Directions;
-                })
-                .catch((err) => {
-                    console.warn(
-                        "ошибка storeCourseContent, getDirections: ",
-                        err
-                    );
-                });
-        },
+	state: () => ({
+		courseId: 0,
+		testType: "",
+		generalSettings: {} as IGeneralCourseSettings,
+		directions: [] as IDirection[],
+		questions: [] as ICourseContentQuestions[],
+	}),
+	actions: {
+		async getDirections(courseId: number) {
+			return axios
+				.get(`admin/v1/Course/${courseId}`)
+				.then((response) => {
+					const data = response.data as IDirectionResponse;
+					this.directions = data.Directions;
+				})
+				.catch((err) => {
+					console.warn(
+						"ошибка storeCourseContent, getDirections: ",
+						err
+					);
+				});
+		},
 
-        async getCourseContent(id: string) {
-            return await axios
-                .get<ICourseContent>(`admin/v1/Testing/${id}`)
-                .then((response) => {
-                    const { data } = response;
-                    this.courseId = data.courseId;
-                    this.testType = data.category;
-                    console.log(data.category);
+		async getCourseContent(id: string) {
+			return await axios
+				.get<ICourseContent>(`admin/v1/Testing/${id}`)
+				.then((response) => {
+					const { data } = response;
+					this.courseId = data.courseId;
+					this.testType = data.category;
 
-                    this.questions = data.questions.map((item) => ({
-                        ...item,
-                        content: item.content ?? "",
-                    }));
-                    this.generalSettings = {
-                        title: data.title,
-                        cutScorePercentages: data.cutScorePercentages,
-                    };
-                    return response;
-                })
-                .catch((error) => {
-                    console.log(error);
-                    throw error;
-                });
-        },
-        async patchCourseContent(
-            id: string,
-            formData: { title?: string; cutScorePercentages?: number }
-        ) {
-            return await axios
-                .patch(`admin/v1/Testing/${id}`, formData)
-                .then((response) => {
-                    return response;
-                })
-                .catch((error) => {
-                    throw error;
-                });
-        },
+					this.questions = data.questions.map((item) => ({
+						...item,
+						content: item.content ?? "",
+					}));
+					this.generalSettings = {
+						title: data.title,
+						cutScorePercentages: data.cutScorePercentages,
+					};
+					return response;
+				})
+				.catch((error) => {
+					console.log(error);
+					throw error;
+				});
+		},
+		async patchCourseContent(
+			id: string,
+			formData: { title?: string; cutScorePercentages?: number }
+		) {
+			return await axios
+				.patch(`admin/v1/Testing/${id}`, formData)
+				.then((response) => {
+					return response;
+				})
+				.catch((error) => {
+					throw error;
+				});
+		},
 
-        async patchQuestion(formData: ICourseContentQuestions) {
-            return await axios
-                .patch(`admin/v1/Question/${formData.id}`, formData)
-                .then((response) => {
-                    console.log("questionUpdated!", formData);
-                    return response;
-                })
-                .catch((error) => {
-                    console.log(error);
-                    return error;
-                });
-        },
+		async patchQuestion(formData: ICourseContentQuestions) {
+			return await axios
+				.patch(`admin/v1/Question/${formData.id}`, formData)
+				.then((response) => {
+					console.log("questionUpdated!", formData);
+					return response;
+				})
+				.catch((error) => {
+					console.log(error);
+					return error;
+				});
+		},
 
-        async addQuestion(testingId: number) {
-            return await axios
-                .post("admin/v1/Question", { testingId })
-                .then((response) => {
-                    console.log(response);
-                    return response;
-                })
-                .finally(() => {});
-        },
-    },
+		async addQuestion(testingId: number) {
+			return await axios
+				.post("admin/v1/Question", { testingId })
+				.then((response) => {
+					console.log(response);
+					return response;
+				})
+				.finally(() => {});
+		},
+	},
 });
