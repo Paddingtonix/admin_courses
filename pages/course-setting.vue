75e0e4d834182997deabebf0e1b5015a14600c20
Aspors <tmieoml@gmail.com>
2024-10-21 13:25:21 +0700

    еще поправил

--- pages/course-setting.vue
+++ pages/course-setting.vue
@@ -322,15 +322,17 @@ export default defineComponent({
                 line-height: rem(24)
 
     &__info
-        width: rem(960)
+        max-width: rem(960)
+        position: relative
         &__fields
-            position: relative
             border-bottom: rem(1) solid $disabled_basic
             @include flex_start()
             &__cell
                 padding: rem(16) rem(8)
-                max-width: 70%
+                flex: 3 1
+                overflow: hidden
                 &:nth-child(1)
+                    flex: 1 0
                     width: rem(272)
                     span
                         color: #5B6C7B
